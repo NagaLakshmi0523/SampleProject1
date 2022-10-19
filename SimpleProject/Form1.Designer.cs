01e9b3433ade72df108c71aab6e4ee111cc0359a
Edhunury <nagalakshmi.edhunury@accenture.com>
2022-10-19 11:41:04 +0530

    first commit

---
+++ SimpleProject/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace SimpleProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbtn1 = new System.Windows.Forms.TextBox();
+            this.txtbtn2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnPluse = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnMInus = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Purple;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(312, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculation Form";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(147, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number 1 :";
+            // 
+            // txtbtn1
+            // 
+            this.txtbtn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbtn1.Location = new System.Drawing.Point(308, 146);
+            this.txtbtn1.Name = "txtbtn1";
+            this.txtbtn1.Size = new System.Drawing.Size(392, 35);
+            this.txtbtn1.TabIndex = 2;
+            // 
+            // txtbtn2
+            // 
+            this.txtbtn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbtn2.Location = new System.Drawing.Point(308, 243);
+            this.txtbtn2.Name = "txtbtn2";
+            this.txtbtn2.Size = new System.Drawing.Size(392, 35);
+            this.txtbtn2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(147, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Number 2 :";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(308, 430);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(392, 35);
+            this.txtResult.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(147, 430);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 32);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Result :";
+            // 
+            // btnPluse
+            // 
+            this.btnPluse.BackColor = System.Drawing.Color.Silver;
+            this.btnPluse.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPluse.Location = new System.Drawing.Point(318, 338);
+            this.btnPluse.Name = "btnPluse";
+            this.btnPluse.Size = new System.Drawing.Size(77, 68);
+            this.btnPluse.TabIndex = 7;
+            this.btnPluse.Text = "+";
+            this.btnPluse.UseVisualStyleBackColor = false;
+            this.btnPluse.Click += new System.EventHandler(this.btnPluse_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.AutoSize = true;
+            this.btnMultiply.BackColor = System.Drawing.Color.Silver;
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(515, 338);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(77, 68);
+            this.btnMultiply.TabIndex = 8;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnMultiply.UseVisualStyleBackColor = false;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnMInus
+            // 
+            this.btnMInus.BackColor = System.Drawing.Color.Silver;
+            this.btnMInus.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMInus.Location = new System.Drawing.Point(416, 338);
+            this.btnMInus.Name = "btnMInus";
+            this.btnMInus.Size = new System.Drawing.Size(77, 68);
+            this.btnMInus.TabIndex = 8;
+            this.btnMInus.Text = "-";
+            this.btnMInus.UseVisualStyleBackColor = false;
+            this.btnMInus.Click += new System.EventHandler(this.btnMInus_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.BackColor = System.Drawing.Color.Silver;
+            this.btnDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(614, 338);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(77, 68);
+            this.btnDivide.TabIndex = 9;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = false;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(944, 546);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMInus);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnPluse);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtbtn2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtbtn1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbtn1;
+        private System.Windows.Forms.TextBox txtbtn2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnPluse;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnMInus;
+        private System.Windows.Forms.Button btnDivide;
+    }
+}
+
---
+++ SimpleProject/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPluse_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(txtbtn1.Text);
+            double num2 = double.Parse(txtbtn2.Text);   
+            double result = num1 + num2;
+            txtResult.Text = result.ToString();
+
+        }
+
+        private void btnMInus_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(txtbtn1.Text);
+            double num2 = double.Parse(txtbtn2.Text);
+            double result = num1 - num2;
+            txtResult.Text = result.ToString();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(txtbtn1.Text);
+            double num2 = double.Parse(txtbtn2.Text);
+            double result = num1 * num2;
+            txtResult.Text = result.ToString();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(txtbtn1.Text);
+            double num2 = double.Parse(txtbtn2.Text);
+            double result = num1 / num2;
+            txtResult.Text = result.ToString();
+        }
+    }
+}
